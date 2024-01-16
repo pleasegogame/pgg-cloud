cd307c3bc76b4a80078d664121082d605ba54464
姚野 <zuiaiyaoye@qq.com>
2024-01-16 17:35:25 +0800

    集成swagger&增加gitignore&修改maven package install问题

--- pgg-service/pgg-service-system/src/main/java/com/pgg/service/system/PggSystemApplication.java
+++ pgg-service/pgg-service-system/src/main/java/com/pgg/service/system/PggSystemApplication.java
@@ -3,11 +3,11 @@ package com.pgg.service.system;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.pgg")
 @MapperScan("com.pgg.*.*.mapper")
-//@SpringBootApplication
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class PggSystemApplication {
 
     public static void main(String[] args) {
--- pgg-service/pgg-service-system/src/main/java/com/pgg/service/system/controller/SystemController.java
+++ pgg-service/pgg-service-system/src/main/java/com/pgg/service/system/controller/SystemController.java
@@ -1,11 +1,14 @@
 package com.pgg.service.system.controller;
 
 import com.pgg.service.system.service.ISystemService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "pgg-system")
 @RestController
 @RequestMapping(value = "system")
 @AllArgsConstructor
@@ -13,12 +16,13 @@ public class SystemController {
 
     private final ISystemService systemService;
 
+    @ApiOperation(value = "system list接口")
     @GetMapping(value = "list")
     public Object list() {
         return systemService.list();
     }
 
-
+    @ApiOperation(value = "system page接口")
     @GetMapping(value = "page")
     public Object page() {
         return systemService.page();
