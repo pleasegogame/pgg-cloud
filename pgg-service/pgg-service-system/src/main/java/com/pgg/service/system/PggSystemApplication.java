dc6439385f06fbd6e24e8933abbac50568dd716d
姚野 <zuiaiyaoye@qq.com>
2024-01-22 16:30:01 +0800

    集成redission

--- pgg-service/pgg-service-system/src/main/java/com/pgg/service/system/PggSystemApplication.java
+++ pgg-service/pgg-service-system/src/main/java/com/pgg/service/system/PggSystemApplication.java
@@ -3,6 +3,7 @@ package com.pgg.service.system;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = "com.pgg")
 @MapperScan("com.pgg.*.*.mapper")
 @SpringBootApplication(scanBasePackages = {"com.pgg"})
+@EnableCaching
 public class PggSystemApplication {
 
     public static void main(String[] args) {
